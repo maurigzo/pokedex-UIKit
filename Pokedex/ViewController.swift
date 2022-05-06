9ddefd707b755aaf8f6449e194800888ff57ebf7
Mauri <mauri@Mauris-MacBook-Air.local>
2022-05-06 18:11:06 -0300

    Setup project and add Pokedex TableView

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Mauri on 06/05/2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDelegate {}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
